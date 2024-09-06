53b5c95cfcff5050a5a10d511ea50e39df1757ac
Mille Elfver <mille.elfver98@gmail.com>
2024-09-06 13:04:24 +0200

    added todoservice

---
+++ Shared.Library/Services/TodoService.cs
@@ -0,0 +1,39 @@
+﻿using Shared.Library.Models;
+
+namespace Shared.Library.Services;
+
+public class TodoService
+{
+	private readonly List<TodoItem> _todos = [];
+
+	public IEnumerable<TodoItem> GetTodos() => _todos;
+
+	public bool AddToList(TodoItem todoItem)
+	{
+		try
+		{
+			if (!string.IsNullOrEmpty(todoItem.Activity))
+			{
+				_todos.Add(todoItem);
+				return true;
+			}
+		}
+		catch { }
+		return false;
+	}
+
+	public bool RemoveFromList(string id)
+	{
+		try
+		{
+			var todoItem = _todos.FirstOrDefault(x => x.Id == id);
+			if (todoItem != null)
+			{
+				_todos.Remove(todoItem);
+				return true;
+			}
+		}
+		catch { }
+		return false;
+	}
+}
