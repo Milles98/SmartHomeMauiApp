9f02846cf3c92999ddeb2daa1b3073770beb6957
Mille Elfver <mille.elfver98@gmail.com>
2024-09-06 14:30:07 +0200

    progress

---
+++ Shared.Library/Services/DeviceManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Shared;
+
+namespace Shared.Library.Services;
+
+public class DeviceManager
+{
+	private readonly RegistryManager _registryManager;
+	private readonly ServiceClient _serviceClient;
+
+	public DeviceManager(string connectionString)
+	{
+		_registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+		_serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+	}
+
+	public async Task GetDevices(string query)
+	{
+		var q = _registryManager.CreateQuery(query);
+		var deviceTwins = new List<Twin>();
+
+		foreach (var twin in await q.GetNextAsTwinAsync())
+		{
+			deviceTwins.Add(twin);
+		}
+	}
+}
