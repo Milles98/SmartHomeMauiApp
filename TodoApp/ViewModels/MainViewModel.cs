62aa9f46f28f4d137263a35abbfae29a7700195f
Mille Elfver <mille.elfver98@gmail.com>
2024-09-06 10:28:34 +0200

    added logic to mainviewmodel

--- TodoApp/ViewModels/MainViewModel.cs
+++ TodoApp/ViewModels/MainViewModel.cs
@@ -1,7 +1,22 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Shared.Library.Models;
+using System.Collections.ObjectModel;
 
 namespace TodoApp.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
+	[ObservableProperty]
+	private TodoItem _todoItem = new();
+
+	[ObservableProperty]
+	private ObservableCollection<TodoItem> _todoItems = new();
+
+	[RelayCommand]
+	private void SaveTodo()
+	{
+		TodoItems.Clear();
+		TodoItems.Add(TodoItem);
+	}
 }
