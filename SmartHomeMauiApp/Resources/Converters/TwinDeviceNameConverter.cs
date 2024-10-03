af0e5dd647454aeeec628c37bef5e4f737389144
Mille Elfver <mille.elfver98@gmail.com>
2024-10-03 08:21:56 +0200

    Add TwinDeviceNameConverter and update MainPage display
    Added TwinDeviceNameConverter to App.xaml resource dictionary.
    Modified MainPage.xaml to display device name instead of last activity.
    Created TwinDeviceNameConverter.cs to implement IValueConverter for extracting device names from twin properties.

---
+++ SmartHomeMauiApp/Resources/Converters/TwinDeviceNameConverter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Devices.Shared;
+using System.Globalization;
+
+namespace SmartHomeMauiApp.Resources.Converters;
+
+public class TwinDeviceNameConverter : IValueConverter
+{
+    public object Convert(object? value, Type? targetType, object? parameter, CultureInfo culture)
+    {
+        var twin = value as Twin;
+        if (twin != null)
+        {
+            if (twin.Properties.Desired.Contains("deviceName"))
+            {
+                return twin.Properties.Desired["deviceName"].ToString();
+            }
+            else if (twin.Properties.Reported.Contains("deviceName"))
+            {
+                return twin.Properties.Reported["deviceName"].ToString();
+            }
+        }
+        return "Unknown Device";
+    }
+
+    public object ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
